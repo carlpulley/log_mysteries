57e2a219f84265258d13202d579cbf8d06b30743
Huddersfield University <huddersfield@bender.firewall.acme-server.com>
2010-10-02 11:28:45 +0100

    db:seed downloads santized_log.zip and extracts www-access.log. Unit tests added to ensure that download is correct and www-access.log is correctly extracted.

---
+++ test/unit/santitized_log_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class SanitizedLogTest < ActiveSupport::TestCase
+  context "sanitized_log.zip" do
+    setup do
+      @sanitized_log = "evidence/sanitized_log.zip"
+    end
+    
+    should "be downloaded" do
+      assert FileTest.file?(@sanitized_log)
+    end
+    
+    should "have a valid SHA1" do
+      assert_equal "5d317ecf8147cafc0239166e47139afea3200c5b", Digest::SHA1.hexdigest(open(@sanitized_log, "r").read)
+    end
+    
+    should "have the correct file type" do
+      assert_match "Zip archive data", `/usr/bin/file #{@sanitized_log}`
+    end
+  end
+end
---
+++ test/unit/www_access_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class SanitizedLogTest < ActiveSupport::TestCase
+  context "www-access.log" do
+    setup do
+      @www_access = "evidence/sanitized_log/apache2/www-access.log"
+    end
+    
+    should "be extracted" do
+      assert FileTest.file?(@www_access)
+    end
+    
+    should "have a valid SHA1" do
+      assert_equal "a5e7274843bbf4a617bbdc557425d43b7b4508c2", Digest::SHA1.hexdigest(open(@www_access, "r").read)
+    end
+    
+    should "have the correct file type" do
+      assert_match "ASCII text", `/usr/bin/file #{@www_access}`
+    end
+  end
+end
