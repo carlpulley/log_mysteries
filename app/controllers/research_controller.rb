30c32111ac06a7b5cdb90df17d73d5ec07178b7e
Carl Pulley <c.j.pulley@hud.ac.uk>
2010-12-03 23:40:36 +0000

    Refactored stop badware and blacklist lookups into research controller

--- app/controllers/research_controller.rb
+++ app/controllers/research_controller.rb
@@ -100,4 +100,33 @@ class ResearchController < ApplicationController
       render "index"
     end
   end
+  
+  # FIXME: function should only be called in response to update events on a record
+  def stop_badware_lookup
+    # TODO: place contents of http://www.stopbadware.org/reports/asn/#{self.asn}.csv into DB
+    url = "http://www.stopbadware.org/reports/asn/#{self.asn}.csv"
+    page = open(url).read
+    if page =~ /^Date,Partner Reports,Community Reports\n/
+      webpage = WebPage.find_or_create_by_url("GET #{url}", :page => page)
+      webpage.page.split("\n").each do |line|
+        
+      end
+    end
+  end
+  
+  # FIXME: function should only be called in response to update events on a record
+  def blacklist_lookup
+    uri = URI.parse("http://whatismyipaddress.com/blacklist-check")
+    url = "POST #{uri.to_s}?LOOKUPADDRESS=#{self.value}"
+    page = add_resource_tagging(uri, Net::HTTP.post_form(uri, {:LOOKUPADDRESS => self.value}).body)
+    webpage = WebPage.find_or_create_by_url(url, :page => page.to_s)
+    legend = { @@hash => { "eb10c44992742f4653be081db009f453b7c5b646" => "Not Listed", "af1c759f45faee863d9169e20400f0f0a25a3630" => "Listed", "3ebd550fd9ec72bd02601ce2acbb3e408f8fa020" => "Timeout Error", "6c9e96b886b2185fd3a1565cfe5aae3981ec4afb" => "Offline" },
+      :ssdeep => { "6:6v/lhPfgl0znDspU9oUyJNf6WRaLERS8ISzFLtVlkLFhaqYBpZajp:6v/7oLo/yXtQYNlkjaZBM" => "Not Listed", "6:6v/lhPfgl0znDsptftJnKSNKyzjB/k2FQWhKvid09kzbanH/NKTNcpP/+p:6v/7oLrtJNP+ud09kzESNj" => "Listed", "6:6v/lhPfgl0znDsptBlc4DETjB/QhDgqwZjLvoqXkaR/O93oKFscGzd0T0GGYnf/N:6v/7oLlMTjyhDwZjLtR/OSKWLaXnf1" => "Timeout Error", "6:6v/lhPfgl0znDsptyO039SoNZIWAX6b2BB/gJ/aRVJFFKttSKo+3/dXsGjP+V1Jh:6v/7oLpYSxfXM2B6VaRVJFFKEp+vSdh" => "Offline" }
+    }
+    results = page.css("td").select { |item| item.try(:at_css, 'a') && item.try(:at_css, 'img') }.map { |item| puts "#{item}" if item.at_css("a").nil?; [item.at_css("a").text, item.at_css("a")['href'], legend[@@hash][item.at_css("img")[@@hash]]] }
+    results.each do |site, ref, status|
+      self.blacklists << Blacklist.find_or_create_by_site(site, :status => status, :web_page => webpage, :reference => ref)
+    end
+  end
+  
 end
--- app/models/ip_address.rb
+++ app/models/ip_address.rb
@@ -24,7 +24,6 @@ class IpAddress < ActiveRecord::Base
   
   def before_create
     asn_lookup
-    blacklist_lookup
   end
   
   validates_presence_of :value
@@ -41,24 +40,6 @@ class IpAddress < ActiveRecord::Base
       self.cc = result[2]
       self.registry = result[3]
       self.allocated_at = DateTime.strptime(result[4][0..-2], "%Y-%m-%d")
-    end
-  end
-  
-  def stop_badware_lookup
-    # TODO: place contents of http://www.stopbadware.org/reports/asn/#{self.asn}.csv into DB
-  end
-  
-  def blacklist_lookup
-    uri = URI.parse("http://whatismyipaddress.com/blacklist-check")
-    url = "POST #{uri.to_s}?LOOKUPADDRESS=#{self.value}"
-    page = add_resource_tagging(uri, Net::HTTP.post_form(uri, {:LOOKUPADDRESS => self.value}).body)
-    webpage = WebPage.find_or_create_by_url(url, :page => page.to_s)
-    legend = { @@hash => { "eb10c44992742f4653be081db009f453b7c5b646" => "Not Listed", "af1c759f45faee863d9169e20400f0f0a25a3630" => "Listed", "3ebd550fd9ec72bd02601ce2acbb3e408f8fa020" => "Timeout Error", "6c9e96b886b2185fd3a1565cfe5aae3981ec4afb" => "Offline" },
-      :ssdeep => { "6:6v/lhPfgl0znDspU9oUyJNf6WRaLERS8ISzFLtVlkLFhaqYBpZajp:6v/7oLo/yXtQYNlkjaZBM" => "Not Listed", "6:6v/lhPfgl0znDsptftJnKSNKyzjB/k2FQWhKvid09kzbanH/NKTNcpP/+p:6v/7oLrtJNP+ud09kzESNj" => "Listed", "6:6v/lhPfgl0znDsptBlc4DETjB/QhDgqwZjLvoqXkaR/O93oKFscGzd0T0GGYnf/N:6v/7oLlMTjyhDwZjLtR/OSKWLaXnf1" => "Timeout Error", "6:6v/lhPfgl0znDsptyO039SoNZIWAX6b2BB/gJ/aRVJFFKttSKo+3/dXsGjP+V1Jh:6v/7oLpYSxfXM2B6VaRVJFFKEp+vSdh" => "Offline" }
-    }
-    results = page.css("td").select { |item| item.try(:at_css, 'a') && item.try(:at_css, 'img') }.map { |item| puts "#{item}" if item.at_css("a").nil?; [item.at_css("a").text, item.at_css("a")['href'], legend[@@hash][item.at_css("img")[@@hash]]] }
-    results.each do |site, ref, status|
-      self.blacklists << Blacklist.find_or_create_by_site(site, :status => status, :web_page => webpage, :reference => ref)
     end
   end
   
