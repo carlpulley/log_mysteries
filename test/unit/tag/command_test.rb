9508732da1fc7098691fdd559eea575155a3d567
Carl Pulley <c.j.pulley@hud.ac.uk>
2011-01-31 10:14:54 +0000

    Added in unit tests to ensure that taggings are peformed correctly

---
+++ test/unit/tag/command_test.rb
@@ -0,0 +1,31 @@
+#    Log Mysteries: partial answer for Honeynet challenge
+#    Reference: http://honeynet.org/challenges/2010_5_log_mysteries
+#    Copyright (C) 2010  Dr. Carl J. Pulley
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'test_helper'
+
+class CommandTest < ActiveSupport::TestCase
+  context "Sudo model" do
+    setup do
+      ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations["development"])
+    end
+    
+    should "have correct tagging of Sudo instances with command tagging names" do
+      command_list = Sudo.command_counts.order(:name).map { |t| [t.name, t.taggings.order(:id).map { |ts| ts.taggable.to_s }] }
+      assert_equal "750b593489adbb2a72acde5d608430f3d13e388a", Digest::SHA1.hexdigest(command_list.to_s)
+    end
+  end
+end
