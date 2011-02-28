0256c05b147b6fc3306519d86167b40e78e582c3
Carl Pulley <c.j.pulley@hud.ac.uk>
2011-02-28 15:42:45 +0000

    Fixes to get signature matching graphs working

--- db/migrate/20101025103015_add_state_column_to_auth_table.rb
+++
@@ -1,9 +0,0 @@
-class AddStateColumnToAuthTable < ActiveRecord::Migration
-  def self.up
-    add_column :auths, :state, :string
-  end
-
-  def self.down
-    remove_column :auths, :state
-  end
-end
